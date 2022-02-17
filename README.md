424f6f69589ffb0a5bf3438854c16b6acb1e1a07
andrew-suh <andrew.suh@ibm.com>
2022-02-17 15:39:00 -0800

    Made changes to prereun script and added to readme

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # docker-core-setup
 
-In the Vagrant file, change the privay=te ip address to any addres based on the range: 192.168.56.0/21. 
+
+The main purpose of this repo is to create a Vagrant vm with Docker inside to connect your local terminal with. The main target audience is MacOS as you can install docker-ce in Linux via distro manager. You will need to install the Docker CLI, Docker Compose, Vagrant, and Vagrant's vb guest.
+
+The hypervisor used for Vagrant will be Virtualbox so you will need ot install Virtual box and the extention pack for Virtualbox.
+
+## Prerequisites
+
+For MacOS:
+
+```
+brew install --cask virtualbox
+brew install --cask virtualbox-extension-pack
+
+brew install vagrant
+vagrant plugin install vagrant-vbguest
+
+brew install docker
+brew install docker-compose
+```
+
+In the Vagrant file, there is a private ip address('###.###.#.#') to any address based on the range: 192.168.56.0/21. The pre-runs cript you need to run will ask for an ip address and change it for you. Make sure the format is correct as you enter the new ip address.
+
+
+## Pre-run script
+
+Inside this script is a couple settings needed to connect to the Vagrant vm via Docker CLI. I have commented out the aliases that I use to make it wasier to control the Vgrant vm state in any directory I'm in. 
+
+You are encourgaed to modify the aliases however you wish. I also welcome any merges that adds to the aliases to make them more useful. 
--- pre-run.sh
+++ pre-run.sh
@@ -14,20 +14,17 @@ select shell in "${options[@]}";do
       cat << EOF >> ~/.bashrc
       export DOCKER_HOST='tcp://${ipaddr}:2375'
       export DOCKER_TLS_VERIFY=
-      alias docker-up='vagrant up $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
-      alias docker-down='vagrant halt $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
-      alias docker-status='vagrant global-status | grep docker.local | cut -f 5 -d " "'
-      alias docker-reload='vagrant reload $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
       EOF
 
     "Zsh")
       cat << EOF >> ~/.zshrc
       export DOCKER_HOST='tcp://${ipaddr}:2375'
       export DOCKER_TLS_VERIFY=
-      alias docker-up='vagrant up $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
-      alias docker-down='vagrant halt $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
-      alias docker-status='vagrant global-status | grep docker.local | cut -f 5 -d " "'
-      alias docker-reload='vagrant reload $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
       EOF
   esac
 done
+
+ #alias docker-up='vagrant up $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
+ #alias docker-down='vagrant halt $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
+ #alias docker-status='vagrant global-status | grep docker.local | cut -f 5 -d " "'
+ #alias docker-reload='vagrant reload $(vagrant global-status | grep docker.local | cut -f 1 -d " ")'
